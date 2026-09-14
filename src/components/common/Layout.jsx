1b36bf5211f4d9e9bc1410984385e257621bb0e4
CharltonAwesome2 <220483418@mycput.ac.za>
2026-09-14 07:30:44 +0200

    Project display name update

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -16,7 +16,7 @@ const Layout = () => {
         <div className={styles.headerInner}>
           <Link to="/" className={styles.logo}>
             <img src={import.meta.env.BASE_URL + "cput-logo.png"} alt="CPUT" className={styles.logoImg} />
-            <span className={styles.logoText}>Community Store</span>
+            <span className={styles.logoText}>TrustHive</span>
           </Link>
 
           <nav className={styles.nav}>
